bbc4737f9cfd65d39d8472c475950fc4981698d6
Ikeda <brandon_ikeda@hotmail.com>
2017-12-05 14:22:11 -0800

    Added Firebase

--- App.js
+++ App.js
@@ -5,10 +5,23 @@ import {
   Text,
   View
 } from 'react-native';
+import firebase from 'firebase';
 import TeamList from './src/components/TeamList';
 import Router from './src/Router';
 
+
 export default class App extends Component<{}> {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBokjlguPSIJwjVrJjEadB5zbPiL7bvU7o',
+      authDomain: 'nhlci-2a4d7.firebaseapp.com',
+      databaseURL: 'https://nhlci-2a4d7.firebaseio.com',
+      projectId: 'nhlci-2a4d7',
+      storageBucket: 'nhlci-2a4d7.appspot.com',
+      messagingSenderId: '622182062488'
+    });
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
