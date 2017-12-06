e5b4ed89bf83d043a286dec6affdffd6a8865d88
Ikeda <brandon_ikeda@hotmail.com>
2017-12-05 19:26:35 -0800

    Added Router

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Scene, Router} from 'react-native-router-flux';
 import TeamList from './components/TeamList';
+import LoginForm from './components/LoginForm';
 
 const RouterComponent = () => {
   return (
---
+++ src/components/LoginForm.js
@@ -0,0 +1,28 @@
+// import React, { Component } from 'react';
+// import {View } from 'react-native';
+// import {Button, Card, CardSection} from './';
+//
+// class LoginForm extends Component {
+//   render() {
+//     return (
+//       <Card>
+//         <CardSection>
+//           <View style={styles.thumbnailContainerStyle}>
+//             <Image style={styles.thumbnailStyle}
+//                    resizeMode='contain'
+//                    source={{require: ('../img/nhlLogo')}}
+//             />
+//           </View>
+//         </CardSection>
+//         <CardSection />
+//         <CardSection>
+//           <Button>
+//             Log In
+//           </Button>
+//         </CardSection>
+//       </Card>
+//     );
+//   }
+// }
+//
+// export default LoginForm;
